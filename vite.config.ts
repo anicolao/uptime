5b2436b1d306215a5fe055a8463b62ca91511b13
Alex Nicolaou <anicolao@gmail.com>
2026-02-14 11:50:19 -0500

    Fix(ci): Relax COOP/COEP headers to allow auth popup

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		headers: {
+			'Cross-Origin-Opener-Policy': 'unsafe-none',
+			'Cross-Origin-Embedder-Policy': 'unsafe-none'
+		}
+	}
 });
